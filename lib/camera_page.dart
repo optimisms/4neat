8e7e9428c7cbc356fb29745cffc7ced4dde81652
optimisms <optimisms@users.noreply.github.com>
2022-11-12 13:07:41 -0700

    Added camera screen!

---
+++ lib/camera_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+
+class CameraPage extends StatefulWidget {
+  final List<CameraDescription>? cameras;
+  const CameraPage({Key? key, required this.cameras}) : super(key: key);
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late CameraController _cameraController;
+
+  Future initCamera(CameraDescription cameraDescription) async {
+// create a CameraController
+    _cameraController =
+        CameraController(cameraDescription, ResolutionPreset.high);
+// Next, initialize the controller. This returns a Future.
+    try {
+      await _cameraController.initialize().then((_) {
+        if (!mounted) return;
+        setState(() {});
+      });
+    } on CameraException catch (e) {
+      debugPrint("camera error $e");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // initialize the rear camera
+    initCamera(widget.cameras![0]);
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: _cameraController.value.isInitialized
+            ? CameraPreview(_cameraController)
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
+      ),
+    );
+  }
+}
